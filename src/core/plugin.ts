2fb83ba68a09eab015c61c0c2f1095aad401e219
bagidea <bagideachannel@gmail.com>
2025-10-15 18:42:23 +0700

    setup room

--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -4,6 +4,19 @@ import {
 
 class Plugin {
     static imageBitmapLoader: ImageBitmapLoader = new ImageBitmapLoader().setOptions({ imageOrientation: "flipY" }).setPath(self.location.origin+"/")
+
+    static loadImageBitmap = (url: string): Promise<ImageBitmap> => {
+        return new Promise((resolve, reject) => {
+            Plugin.imageBitmapLoader.load(
+                url,
+                (data: ImageBitmap) => {
+                    resolve(data)
+                },
+                undefined,
+                (err: unknown) => reject(err)
+            )
+        })
+    }
 }
 
 export default Plugin
--- src/main/scenes/menu.ts
+++ src/main/scenes/menu.ts
@@ -2,49 +2,78 @@ import Plugin from "@engine/plugin"
 import Scene from "@engine/scene"
 
 import {
-    DirectionalLight,
     MathUtils,
     Mesh,
     MeshStandardNodeMaterial,
+    NoColorSpace,
     PlaneGeometry,
     RepeatWrapping,
+    SRGBColorSpace,
     Texture
 } from "three/webgpu"
 
 import {
-    texture
+    texture,
+    bumpMap,
+    float,
+    color
 } from "three/tsl"
 
 class Menu extends Scene {
-    start = () => {
-        // Ground
+    start = async() => {
+        // Ground and Top
 
         const groundGeometry: PlaneGeometry = new PlaneGeometry(10, 10)
         const groundMaterial: MeshStandardNodeMaterial = new MeshStandardNodeMaterial()
 
+        const groundImageBitmap: ImageBitmap = await Plugin.loadImageBitmap("textures/lavatile.jpg")
+
+        const groundTexture: Texture = new Texture(groundImageBitmap)
+        this.setupTexture(groundTexture, 2, 2, NoColorSpace)
+
+        groundMaterial.colorNode = color(1, 1, 1)
+        groundMaterial.roughnessNode = texture(groundTexture).mul(0.4)
+        groundMaterial.metalnessNode = float(1)
+
         const ground: Mesh = new Mesh(groundGeometry, groundMaterial)
         ground.rotation.x = MathUtils.degToRad(-90)
 
-        this.scene.add(ground)
+        const top: Mesh = ground.clone()
+        top.position.set(0, 5, 0)
+        top.rotation.x = MathUtils.degToRad(90)
+
+        this.scene.add(ground, top)
 
         // Walls
 
         const wallGeometry: PlaneGeometry = new PlaneGeometry(10, 5)
         const wallMaterial: MeshStandardNodeMaterial = new MeshStandardNodeMaterial()
 
-        Plugin.imageBitmapLoader.load("textures/brick_diffuse.jpg", (data: ImageBitmap) => {
-            const tex: Texture = new Texture(data)
-            tex.wrapS = RepeatWrapping
-            tex.repeat.set(2, 1)
-            tex.needsUpdate = true
+        const wallDiffuseImageBitmap: ImageBitmap = await Plugin.loadImageBitmap("textures/brick_diffuse.jpg")
+        const wallBumpImageBitmap: ImageBitmap = await Plugin.loadImageBitmap("textures/brick_bump.jpg")
 
-            wallMaterial.colorNode = texture(tex)
+        const wallDiffuseTexture: Texture = new Texture(wallDiffuseImageBitmap)
+        this.setupTexture(wallDiffuseTexture, 2, 1)
 
-            const wallBack: Mesh = new Mesh(wallGeometry, wallMaterial)
-            wallBack.position.set(0, 2.5, -5)
+        const wallBumpTexture: Texture = new Texture(wallBumpImageBitmap)
+        this.setupTexture(wallBumpTexture, 2, 1)
 
-            this.scene.add(wallBack)
-        })
+        wallMaterial.colorNode = texture(wallDiffuseTexture)
+        wallMaterial.normalNode = bumpMap(texture(wallBumpTexture))
+        wallMaterial.roughnessNode = float(0.3)
+
+        const wallBack: Mesh = new Mesh(wallGeometry, wallMaterial)
+        wallBack.position.set(0, 2.5, -5)
+
+        const wallLeft: Mesh = wallBack.clone()
+        wallLeft.position.set(-5, 2.5, 0)
+        wallLeft.rotation.y = MathUtils.degToRad(90)
+
+        const wallRight: Mesh = wallBack.clone()
+        wallRight.rotation.y = MathUtils.degToRad(-90)
+        wallRight.position.set(5, 2.5, 0)
+
+        this.scene.add(wallBack, wallLeft, wallRight)
 
         // Camera
 
@@ -52,11 +81,14 @@ class Menu extends Scene {
         this.cameraLookAt(0, 1, 0)
 
         // Lightings
+    }
 
-        const directionalLight: DirectionalLight = new DirectionalLight(0xffffff, 1)
-        directionalLight.position.set(10, 10, 10)
-
-        this.scene.add(directionalLight)
+    private setupTexture = (texture: Texture, x: number, y: number, colorSpace: string = SRGBColorSpace) => {
+        texture.wrapS = RepeatWrapping
+        texture.wrapT = RepeatWrapping
+        texture.repeat.set(x, y)
+        texture.colorSpace = colorSpace
+        texture.needsUpdate = true
     }
 
     update = (_time: DOMHighResTimeStamp, _delta: number) => {
