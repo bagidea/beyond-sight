3b3db60058ee7a75b689a583f2d03f8a6a1d121d
bagidea <bagideachannel@gmail.com>
2025-10-21 05:01:32 +0700

    fix error build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import tsconfigPaths from "vite-tsconfig-paths"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@engine': path.resolve(__dirname, 'src/core'),
+    },
+  },
 })
